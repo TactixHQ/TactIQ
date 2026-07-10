4179b0e593123b399482c6d14bfd7f14c12cc1ef
TactixHQ <Aidan.Matt@icloud.com>
2026-07-10 21:12:26 +0000

    Fix AI Coach input visibility issue by improving mobile layout

--- artifacts/tactiq/src/components/TacticsBoard.tsx
+++ artifacts/tactiq/src/components/TacticsBoard.tsx
@@ -1505,7 +1505,7 @@ export default function TacticsBoard({
         </div>
 
         {/* Messages list */}
-        <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        <div className="flex-1 min-h-0 overflow-y-auto p-4 space-y-4">
           {aiChat.map((msg, idx) => (
             <div 
               key={idx}
